0e07fcedc8e62af00dd91d6534f9cab33980d208
joseph chaudhari <chaudharijoe@gmail.com>
2021-01-10 17:19:17 -0600

    second committ

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,41 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
+  // get user inputs about the password options
+// we have only numeric and lower case
+// ask for the lenght of the password
+  gatherInput();
+
+  return "dummyPa$$word"
 
 }
 
+function gatherInput() {
+  // see how to convert strings to numbers
+  var passwordLength = prompt("How long would you like your password to be?");
+  // add if statement @ boundary conditions 
+  var holdsNumeric = confirm("Do you want password to contain numberic values?");
+
+  var holdsLowerCase = confirm("Do you want LowerCase Letters in password?");
+
+  var holdsUpperCase = confirm("Do you want Upper Case letters in password?");
+
+  var holdsSpecChars = confirm("Do you want Special Characters in your password?");
+
+  var userInputs = {
+    passwordLengthKey: passwordLength,
+    holdsNumbericKey: holdsNumeric,
+    holdsLowerCaseKey: holdsLowerCase,
+    holdsUpperCaseKey: holdsUpperCase,
+    holdsSpecCharsKey: holdsSpecChars,
+  };
+
+  console.log("These are the user inputs", userInputs);
+
+  return userInputs;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-document.getElementById("btn").addEventListener("click", function()) {
-  document.getElementById("demo").innerHTML = "Hello World";
-});
