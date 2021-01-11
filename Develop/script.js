348603f475bdbf810cfd83b39dcc5b943a18bc08
joseph chaudhari <chaudharijoe@gmail.com>
2021-01-11 06:52:18 -0600

    third commit

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,24 @@ function generatePassword() {
   // get user inputs about the password options
 // we have only numeric and lower case
 // ask for the lenght of the password
+  var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var upperCase = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specChars = ["!", "@", "$", "%", "^", "&", "*"];
+  // generate and log a random value from each array above
+  var numericRandom = Math.floor(Math.random() * numeric.length);
+  console.log(random,numeric[random]);
+
+  var upperCaseRandom = Math.floor(Math.random() * upperCase.length);
+  console.log(random,upperCase[random]);
+
+  var lowerCaseRandom = Math.floor(Math.random() * lowerCase.length);
+  console.log(random,lowerCase[random]);
+
+  var specCharsRandom = Math.floor(Math.random() * specChars.length);
+  console.log(random,specChars[random]);
+  
+
   gatherInput();
 
   return "dummyPa$$word";
@@ -24,7 +42,7 @@ function gatherInput() {
   var passwordLength = prompt("How long would you like your password to be?");
   // add if statement implementing boundary conditions for password length
   if (passwordLength > 8, passwordLength < 128) {
-    passwordLength = passwordLength;
+    passwordLength = ;
   }
   else {
     passwordLength = "Please Select a Password Length between 8 and 128 characters";
